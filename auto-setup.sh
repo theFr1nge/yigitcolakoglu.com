714896f6a520291f3520e572ef58d8f6a837004f
Yigit Colakoglu <yigitcolakoglu@hotmail.com>
2021-04-09 16:44:24 +0300

    Moved yass

--- auto-setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bash <(curl -s https://raw.githubusercontent.com/theFr1nge/dotfiles/main/arch-setup/arch-setup.sh)
---
+++ auto-yass.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+bash <(curl -s https://raw.githubusercontent.com/theFr1nge/YAASS/main/yaass.sh)
